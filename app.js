ed8dab2a5f801614959f7510f804f39f12d67784
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-07-11 22:08:47 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.listen(3000)
