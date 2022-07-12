e92cf2502f4101a4d0c927cedd4591e2763bf141
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-07-11 22:58:01 -0300

    correcting error in connection with the database

--- app.js
+++ app.js
@@ -5,16 +5,34 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const app = express();
+//Config JSON Response
+app.use(express.json())
 
 const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASSWORD
+const dbPassword = process.env.DB_PASS
 
+// Open Route Public
 app.get('/', (req, res) => {
     res.status(200).json({message: "Bem vindo a nossa API!"})
 })
 
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.wkgoqrv.mongodb.net/?retryWrites=true&w=majority`).then().catch((err) => {
-    app.listen(3000)
-    console.log("Connect with data base!")
 
-}).catch((err) => console.log(err))
+//Register User
+app.post('/auth/register', async(req, res) => {
+    const { name, email, password, confirmPassword } = req.body;
+
+    //validations
+    if(!name){
+        return res.status(422).json({message: "O nome é obrigatório!"})
+    }
+
+})
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.wkgoqrv.mongodb.net/?retryWrites=true&w=majority`)
+        .then(() => {
+            app.listen(3000)
+            console.log("Connect with data base!")
+        }).catch((err) => {
+    
+
+})
