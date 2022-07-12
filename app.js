fa2f5cebe63503030a7188a9a65c37493949710c
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-07-11 22:44:55 -0300

    connect with database mongodb

--- app.js
+++ app.js
@@ -6,4 +6,15 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
-app.listen(3000)
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "Bem vindo a nossa API!"})
+})
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.wkgoqrv.mongodb.net/?retryWrites=true&w=majority`).then().catch((err) => {
+    app.listen(3000)
+    console.log("Connect with data base!")
+
+}).catch((err) => console.log(err))
